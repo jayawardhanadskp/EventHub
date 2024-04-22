fb3515a777a366b8e174a0cab96859938f39635d
jayawardhanadskp <kasunpradeep.ds@gmail.com>
2024-04-22 16:21:23 +0530

    firebase api for notification

---
+++ lib/controller/firebase_api.dart
@@ -0,0 +1,20 @@
+
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class FirebaseApi {
+  final _firebaseMessaging = FirebaseMessaging.instance;
+
+  Future<void> initNotifications() async {
+    await _firebaseMessaging.requestPermission();
+    final fCMToken = await _firebaseMessaging.getToken();
+    print('fCM Token: $fCMToken');
+    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+  }
+}
+
+Future<void> handleBackgroundMessage(RemoteMessage message) async {
+  print('Title: ${message.notification?.title}');
+  print('Body: ${message.notification?.body}');
+  print('Payload: ${message.data}');
+}
